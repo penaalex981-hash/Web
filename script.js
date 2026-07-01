3a79b1cc96f3e85e979c3fa48b9074d8ebbd6c40
Tu Nombre <tuemail@example.com>
2026-06-30 23:06:59 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+<script>
+    function toggleProducto(elementoFila) {
+        // Alterna la clase 'abierto' en la fila donde el usuario hizo clic
+        elementoFila.classList.toggle('abierto');
+    }
+</script>
